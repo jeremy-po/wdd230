c37ecd9ae39757a263ec4d965d53f44aba2edaae
jeremy-po <jeremypoliver@hotmail.com>
2024-06-27 22:13:29 -0400

    fixes

--- week03/scripts/bom.js
+++ week03/scripts/bom.js
@@ -4,14 +4,18 @@ const list = document.querySelector('#list');
 
 let chaptersArray = getChapterList() || [];
 
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
+
 button.addEventListener('click', () => {
     if (input.value !== '') {
-        displayList(input.value); 
-        chaptersArray.push(input.value); 
-        setChapterList(); 
-        input.value = ''; 
-        input.focus(); 
-    }
+    displayList(input.value); 
+    chaptersArray.push(input.value); 
+    setChapterList();
+    input.value = ''; 
+    input.focus(); 
+}
 });
 
 function displayList(item) {
@@ -23,29 +27,25 @@ function displayList(item) {
     li.appendChild(deleteButton);
     list.appendChild(li);
 
-    
     deleteButton.addEventListener('click', () => {
-        list.removeChild(li);
-        deleteChapter(li.textContent);
-        input.focus();
-    });
+    list.removeChild(li);
+    deleteChapter(item); 
+    input.focus(); 
+});
 }
 
 function setChapterList() {
     localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
-    console.log('Updated localStorage:', chaptersArray);
 }
 
 function getChapterList() {
-    const chapters = JSON.parse(localStorage.getItem('myFavBOMList'));
-    console.log('Retrieved from localStorage:', chapters);
-    return chapters;
+    const storedChapters = localStorage.getItem('myFavBOMList');
+    return storedChapters ? JSON.parse(storedChapters) : [];
 }
 
 function deleteChapter(chapter) {
-    chapter = chapter.slice(0, chapter.length - 1);
     chaptersArray = chaptersArray.filter(item => item !== chapter);
-    setChapterList();
+    setChapterList(); 
 }
 
 console.log('Initial chaptersArray:', chaptersArray);
