d44a18d917ba9b12e98a8b4a184e1eb2c3bf0db4
jeremy-po <jeremypoliver@hotmail.com>
2024-07-14 00:15:21 -0400

    test

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -45,7 +45,7 @@ function loadMemberDirectory() {
     const gridViewBtn = document.getElementById("grid-view");
     const listViewBtn = document.getElementById("list-view");
 
-    fetch("/chamber/members.json")
+    fetch("wdd230/chamber/data/members.json")
         .then(response => response.json())
         .then(members => displayMembers(members))
         .catch(error => console.error('Error loading members:', error));
